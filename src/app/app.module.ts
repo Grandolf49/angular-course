b2d3fbb1a3dc3c8e9c99d35a5160355a8e474da0
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-04-25 12:39:59 +0530

    menu component created with food menu items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { MatListModule } from '@angular/material/list';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
